657b607d1b2ae881e3bbd5c63e45156cb10e1e48
Nikita <bigbasspoll@gmail.com>
2021-07-13 19:33:18 +0300

    FoodJsUseAxios

--- js/script.js
+++ js/script.js
@@ -159,51 +159,14 @@ window.addEventListener('DOMContentLoaded', ()=>{
         changeToUAH() {
             this.price = this.price * this.transfer;
         }
-        //Метод который рендерит карточку на страницу
-        // render() {
-        //     const element = document.createElement('div'); //создание элемента
-        //     // Проверка rest оператора если он пуст то ставится значение по умолчанию если нет то формируется массив классов
-        //     if(this.classes.length === 0){
-        //         this.element = 'menu__item';
-        //         element.classList.add(this.element);
-        //     } else {
-        //         this.classes.forEach(className => element.classList.add(className));
-        //     } 
-        //     element.innerHTML = `
-        //             <img src="${this.src}" alt="${this.alt}">
-        //             <h3 class="menu__item-subtitle">Меню ${this.title}</h3>
-        //             <div class="menu__item-descr">${this.text}</div>
-        //             <div class="menu__item-divider"></div>
-        //             <div class="menu__item-price">
-        //                 <div class="menu__item-cost">Цена:</div>
-        //                 <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
-        //             </div>
-        //         `; //Наполнение контентом с подставлением свойств класса
-        //     this.parent.append(element); //вставка элемента в родителя
-        // }
     }
-    // функция, которая достает данные из базы
-    const getResource = async (url) => {
-        const res = await fetch(url);
-        if(!res.ok){ //выброс ошибки в случае провального запроса
-            throw new Error(`Could not fetch ${url}, status: ${res.status}}`);
-        }
-        return await res.json();
-    };
-    //Добавление товаров на сайт
-    // getResource('http://localhost:3000/menu')
-    //     .then(data => {
-    //         data.forEach(({img, altimg, title, descr, price}) => {
-    //             new Card(img, altimg, title, descr, price, '.menu .container').render();
-    //         });
-    //     });
-    
-    //Второй способ
-    getResource('http://localhost:3000/menu')
+    // axios 
+
+    axios.get('http://localhost:3000/menu')
         .then(data => createCard(data));
 
     function createCard(data) {
-        data.forEach(({img, altimg, title, descr, price}) =>{
+        data.data.forEach(({img, altimg, title, descr, price}) =>{
             const element = document.createElement('div');
             element.classList.add('menu__item');
             element.innerHTML = `
